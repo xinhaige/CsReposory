499f6c2888dd89deb45eef8a51277c7a8a5c499d
1796827448 <1796827448@qq.com>
2022-07-06 10:34:20 +0800

    初始化

---
+++ CsReposory/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CsReposory
+{
+    public static class MyClass
+    {
+        public static string GetUpper(this string ss)
+        {
+            return ss.ToUpper();
+        }
+        public static string GetLower(this string ss)
+        {
+            return ss.ToLower();
+        }
+    }
+}
